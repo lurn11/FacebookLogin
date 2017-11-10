c58f062d4490ec3c0d2e78d532c5009993b2a905
scott9981 <scott9981@tamu.edu>
2017-11-10 17:56:49 -0600

    working

--- app/src/main/java/com/example/laurenvoigt/facebooklogin/MainActivity.java
+++ app/src/main/java/com/example/laurenvoigt/facebooklogin/MainActivity.java
@@ -4,20 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Call;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtStatus;
+    TextView userName;
+    TextView userId;
     LoginButton login_button;
     CallbackManager callbackManager;
+    ProfilePictureView profilePictureView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +39,9 @@ public class MainActivity extends AppCompatActivity {
     }
     private void initializeControls(){
         callbackManager = CallbackManager.Factory.create();
-        txtStatus = (TextView)findViewById(R.id.txtStatus);
+        userName = (TextView)findViewById(R.id.userName);
+        userId = (TextView)findViewById(R.id.userId);
+        profilePictureView = (ProfilePictureView)findViewById(R.id.picture);
         login_button = (LoginButton)findViewById(R.id.login_button);
     }
 
@@ -37,17 +49,29 @@ public class MainActivity extends AppCompatActivity {
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                txtStatus.setText("Login Sucessful\n" + loginResult.getAccessToken());
-            }
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
 
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("Main", response.toString());
+                                setProfileToView(object);
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
             @Override
             public void onCancel() {
-                txtStatus.setText("Login Cancelled.");
+                userName.setText("Login Cancelled.");
             }
 
             @Override
             public void onError(FacebookException error) {
-                txtStatus.setText("Login Error: " + error.getMessage());
+                userName.setText("Login Error: " + error.getMessage());
             }
         });
 
@@ -56,5 +80,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setProfileToView(JSONObject jsonObject) {
+        try {
+
+            userName.setText("Name:" + jsonObject.getString("name"));
+            userId.setText("UserId: " + jsonObject.getString("id"));
+            profilePictureView.setPresetSize(ProfilePictureView.NORMAL);
+            profilePictureView.setProfileId(jsonObject.getString("id"));
+
+            //infoLayout.setVisibility(View.VISIBLE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
