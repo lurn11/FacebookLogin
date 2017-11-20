2b2b274a9e2782aa38cd2bf72578b799936f0f9f
scott9981 <scott9981@tamu.edu>
2017-11-20 12:30:37 -0600

    doublecheck

--- app/src/main/java/com/example/laurenvoigt/facebooklogin/MainActivity.java
+++ app/src/main/java/com/example/laurenvoigt/facebooklogin/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.laurenvoigt.facebooklogin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telecom.Call;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
